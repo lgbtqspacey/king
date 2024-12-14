d07ccd5c17241389a94a374e48a34803d0f8a81a
Ash Trindade <ashtrindade5@gmail.com>
2024-12-14 10:28:28 -0300

    chore: fix database driver creation

--- composeApp/src/desktopMain/kotlin/com/lgbtqspacey/admin/database/DatabaseDriverFactory.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/lgbtqspacey/admin/database/DatabaseDriverFactory.desktop.kt
@@ -8,7 +8,29 @@ import com.lgbtqspacey.database.AppDatabase
 actual class DatabaseDriverFactory {
     actual suspend fun createDriver(): SqlDriver {
         val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:admin.db")
-        AppDatabase.Schema.create(driver)
+
+        if (isDatabaseEmpty()) {
+            AppDatabase.Schema.create(driver).await()
+        } else {
+            AppDatabase.Schema.create(driver)
+        }
         return driver
+    }
+
+    /**
+     * Checks if the database file is empty.
+     * If `true`, then we should create the `SQL Schema` asynchronously so that it can create the tables.
+     */
+    private fun isDatabaseEmpty(): Boolean {
+        val bashScript = """
+            if [ -s "admin.db" ]; then
+                exit 0
+            else
+                exit 1
+            fi
+        """.trimIndent()
+
+        val process = ProcessBuilder("bash", "-c", bashScript).inheritIO().start().waitFor()
+        return process == 1
     }
 }
